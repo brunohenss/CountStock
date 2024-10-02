29b627f7de92b1baf3e1d245eb7917614c0e12f0
Bruno Henrique dos Santos Silva <brunoricksp@gmail.com>
2024-10-01 22:05:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class CountStock
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o saldo em estoque...");
+        int quantidadeEstoque = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Quantidade de itens que deseja comprar:");
+        int quantidadeCompra = int.Parse(Console.ReadLine());
+
+        bool possivelVenda = quantidadeEstoque >= quantidadeCompra;
+
+        /*   (ABAIXO LINHA DESNECESSÁRIA NO CÓDIGO)
+                  - BRUNO HENRIQUE 26/09/2024
+
+        Console.WriteLine($"É possível realizar a venda: {possivelVenda}");
+        
+        */
+
+        if (possivelVenda)
+        {
+            Console.WriteLine("Venda Realizada...");
+        }
+        else
+        {
+            Console.WriteLine("Desculpe, saldo insuficiente em estoque...");
+        }
+    }
+}
